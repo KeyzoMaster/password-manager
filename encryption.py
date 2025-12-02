a43331b34cf62ac19f8fb1fe2c3e59708424a8b3
GitZo <gitzo@test.com>
2025-12-02 20:06:01 +0000

    Replaced AESCCM with AESGCM in encryption.py

--- encryption.py
+++ encryption.py
@@ -1,15 +1,15 @@
 import secrets
-from cryptography.hazmat.primitives.ciphers.aead import AESCCM
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 def aes_encrypt(message):
     key = secrets.token_bytes(32)
     nonce = secrets.token_bytes(12)
-    aes = AESCCM(key)
+    aes = AESGCM(key)
     ciphertext = nonce + aes.encrypt(nonce, message.encode(),None)
     return key.hex(), ciphertext.hex()
 
 def aes_decrypt(key, message):
     ciphertext = bytes.fromhex(message)
-    aes = AESCCM(bytes.fromhex(key))
+    aes = AESGCM(bytes.fromhex(key))
     plaintext = aes.decrypt(ciphertext[:12], ciphertext[12:], None)
     return plaintext.decode()
